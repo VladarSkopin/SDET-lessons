6cce1ca90afae39d91d8c539812568214f23ad00
Alexander Skopintsev <grudagor@gmail.com>
2023-03-08 16:14:22 -0800

    drag and drop

--- auto_tests/mouse_actions.py
+++ auto_tests/mouse_actions.py
@@ -19,8 +19,34 @@ driver.maximize_window()
 capitals: List[WebElement] = wait.until(ExCon.presence_of_all_elements_located((By.XPATH, "//div[@class='dragableBox' and contains(@id, 'box')]")), "CAPITALS LIST")
 print(len(capitals))
 
+countries: List[WebElement] = wait.until(ExCon.presence_of_all_elements_located((By.XPATH, "//div[@class='dragableBoxRight']")), "COUNTRIES LIST")
+print(len(countries))
+
 act = ActionChains(driver)
 
+country_italy = countries[0]
+country_spain = countries[1]
+country_norway = countries[2]
+country_denmark = countries[3]
+country_south_korea = countries[4]
+country_sweden = countries[5]
+country_us = countries[6]
+
+for capital in capitals:
+    if capital.text.lower() == 'rome':
+        act.move_to_element(capital).click_and_hold().move_to_element(country_italy).release().perform()
+    elif capital.text.lower() == 'madrid':
+        act.move_to_element(capital).click_and_hold().move_to_element(country_spain).release().perform()
+    elif capital.text.lower() == 'oslo':
+        act.move_to_element(capital).click_and_hold().move_to_element(country_norway).release().perform()
+    elif capital.text.lower() == 'copenhagen':
+        act.move_to_element(capital).click_and_hold().move_to_element(country_denmark).release().perform()
+    elif capital.text.lower() == 'seoul':
+        act.move_to_element(capital).click_and_hold().move_to_element(country_south_korea).release().perform()
+    elif capital.text.lower() == 'stockholm':
+        act.move_to_element(capital).click_and_hold().move_to_element(country_sweden).release().perform()
+    elif capital.text.lower() == 'washington':
+        act.move_to_element(capital).click_and_hold().move_to_element(country_us).release().perform()
 
 time.sleep(5)
 driver.quit()
